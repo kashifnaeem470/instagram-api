5b00abad6cbd4b058101b2377243cfbce69057af
Saqib Ejaz <saqib@choosii.us>
2023-05-12 19:46:41 +0500

    first commit

---
+++ src/post/post.entity.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, IsString } from "class-validator";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+Entity('post')
+export class Post extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @IsString()
+    @IsNotEmpty()
+    @Column()
+    title: string;
+
+    @IsString()
+    @Column({ nullable: false, default: '' })
+    picture: string
+}
---
+++ src/user/user.entity.ts
@@ -0,0 +1,38 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+Entity('user')
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @IsString()
+    @IsNotEmpty()
+    @Column({ unique: true })
+    username: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Column()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Column({ unique: true })
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Column({ select: false })
+    password: string;
+
+    @IsNumber()
+    @Column()
+    age: number
+
+    @IsString()
+    @IsOptional()
+    @Column({ default: '' })
+    display_picture: string
+
+}
